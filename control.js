da641bd476a2561b0b7962d8ee38131ba7602bda
Leo Barber <barberlt1@appstate.edu>
2025-04-08 11:41:21 -0400

    Adding starter files

---
+++ control.js
@@ -0,0 +1,11 @@
+class Control {
+    constructor(data) {
+        const size = {
+            /*
+            Define the size of the layout here
+            */
+            width: 1000,
+            height: 1000
+        };
+    }
+}
